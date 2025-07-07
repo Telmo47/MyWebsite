8a5f35c31e11dc0c9469e4a5683d415426315621
Telmo47 <telmobotelho4@gmail.com>
2025-07-07 15:10:48 +0100

    Class Tecnologies established with no PK or FK yet

--- MyWebsite/MyWebsite/Models/Tecnologies.cs
+++ MyWebsite/MyWebsite/Models/Tecnologies.cs
@@ -2,5 +2,16 @@
 {
     public class Tecnologies
     {
+
+        /// <summary>
+        /// Id for the technologies
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the technology 
+        /// </summary>
+        public string Name { get; set; }
+
     }
 }
