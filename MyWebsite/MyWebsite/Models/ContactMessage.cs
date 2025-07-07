3af58f4055e74828b25fd28ca704690d587809a8
Telmo47 <telmobotelho4@gmail.com>
2025-07-07 15:18:12 +0100

    ContactMessage class established with no PK or FK yet

--- MyWebsite/MyWebsite/Models/ContactMessage.cs
+++ MyWebsite/MyWebsite/Models/ContactMessage.cs
@@ -2,5 +2,36 @@
 {
     public class ContactMessage
     {
+
+        /// <summary>
+        /// Id for the contact messages
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the person who sent the message
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Email of the person who sent the message
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Title of the message sent and what it is about
+        /// </summary>
+        public string About { get; set; }
+
+        /// <summary>
+        /// Body of the message sent
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Date when the message was sent
+        /// </summary>
+        public DateTime SentDate { get; set; }
+
     }
 }
