4621c9e17461fadd4a5471a9f1fc8d28b9d8f4ae
Telmo47 <telmobotelho4@gmail.com>
2025-07-07 15:13:12 +0100

    ProjectTecnologies class established for the N-M relation between Projects and Tecnologies, no PK or FK yet

--- MyWebsite/MyWebsite/Models/ProjectTecnologies.cs
+++ MyWebsite/MyWebsite/Models/ProjectTecnologies.cs
@@ -2,5 +2,15 @@
 {
     public class ProjectTecnologies
     {
+
+        /// <summary>
+        /// FK to the Projects table
+        /// </summary>
+        public int ProjectId { get; set; }
+
+        /// <summary>
+        /// FK to the Tecnologies table
+        /// </summary>
+        public int TecnologiesId { get; set; }
     }
 }
