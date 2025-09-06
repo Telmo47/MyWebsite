2b855375b97bd11fab048a79cf6177870d859f17
Telmo47 <telmobotelho4@gmail.com>
2025-09-06 18:01:51 +0100

    SIte almost finished, only missing data, button for github and linkedin and getting published

--- MyWebsite/MyWebsite/Controllers/ProjectsController.cs
+++ MyWebsite/MyWebsite/Controllers/ProjectsController.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyWebsite.Data;
 using MyWebsite.Models;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authorization;
 
 namespace MyWebsite.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    // Only users with "admin" role can access create/edit/delete actions
+    [Authorize(Roles = "admin")]
     public class ProjectsController : Controller
     {
-
         /// <summary>
         /// Reference to the application database context
         /// </summary>
@@ -26,8 +24,8 @@ namespace MyWebsite.Controllers
             _context = context;
         }
 
-
         // GET: Projects
+        // Allow anonymous users to see the list of projects
         [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
@@ -35,21 +33,17 @@ namespace MyWebsite.Controllers
         }
 
         // GET: Projects/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var projects = await _context.Projects
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (projects == null)
-            {
+            var project = await _context.Projects.FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
                 return NotFound();
-            }
 
-            return View(projects);
+            return View(project);
         }
 
         // GET: Projects/Create
@@ -59,118 +53,71 @@ namespace MyWebsite.Controllers
         }
 
         // POST: Projects/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,UrlGithub,UrlSite,ImageUrl,CreationDate")] Projects projects)
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,UrlGithub,UrlSite,ImageUrl,CreationDate")] Projects project)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(projects);
+                _context.Add(project);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(projects);
+            return View(project);
         }
 
         // GET: Projects/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var projects = await _context.Projects.FindAsync(id);
-            if (projects == null)
-            {
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
                 return NotFound();
-            }
-            return View(projects);
 
-            // Saves the data of the object in the session
-            HttpContext.Session.SetInt32("ProjectId", projects.Id);
-            HttpContext.Session.SetString("ProjectTitle", projects.Title);
+            return View(project);
         }
 
         // POST: Projects/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-
-        //Uses the [FromRoute] attribute to bind the id parameter from the route data
-        public async Task<IActionResult> Edit([FromRoute] int id, [Bind("Id,Title,Description,UrlGithub,UrlSite,ImageUrl,CreationDate")] Projects projects)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,UrlGithub,UrlSite,ImageUrl,CreationDate")] Projects project)
         {
-            if (id != projects.Id)
-            {
-                return RedirectToAction("Index");
-            }
-
-            // Are the data I received from the session corresponding to the object sent to the browser?
-            var ProjectId = HttpContext.Session.GetInt32("ProjectId");
-            var ProjectTitle = HttpContext.Session.GetString("ProjectTitle");
-
-            if (ProjectId == null || ProjectTitle.IsNullOrEmpty())
-            {
-                // Took too long to get the data from the session, or the session has expired
-                ModelState.AddModelError(string.Empty, "Project ID not found in session." + "You must restart the process");
-                
-                return View(projects);
-            }
-
-            // Was there any adultation of the object?
-            if (projects.Id != ProjectId || ProjectTitle != "Projects/Edit")
-            {
-                // The user is trying to edit a different project than the one that was loaded in the session
-                return RedirectToAction("Index");
-            }
+            if (id != project.Id)
+                return NotFound();
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    // Saves the data in the database
-                    _context.Update(projects);
+                    // Update project in database
+                    _context.Update(project);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ProjectsExists(projects.Id))
-                    {
+                    if (!_context.Projects.Any(e => e.Id == project.Id))
                         return NotFound();
-                    }
                     else
-                    {
                         throw;
-                    }
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(projects);
+            return View(project);
         }
 
         // GET: Projects/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var projects = await _context.Projects
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (projects == null)
-            {
+            var project = await _context.Projects.FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
                 return NotFound();
-            }
 
-            // Saves the data of the object in the session
-            HttpContext.Session.SetInt32("ProjectId", projects.Id);
-            HttpContext.Session.SetString("ProjectTitle", projects.Title);
-
-            return View(projects);
+            return View(project);
         }
 
         // POST: Projects/Delete/5
@@ -178,35 +125,13 @@ namespace MyWebsite.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var projects = await _context.Projects.FindAsync(id);
-            var ProjectId = HttpContext.Session.GetInt32("ProjectId");
-            var ProjectTitle = HttpContext.Session.GetString("ProjectTitle");
-
-            // Are the data I received from the session corresponding to the object sent to the browser?
-            if (ProjectId == null || ProjectTitle.IsNullOrEmpty())
+            var project = await _context.Projects.FindAsync(id);
+            if (project != null)
             {
-                // Took too long to get the data from the session, or the session has expired
-                ModelState.AddModelError(string.Empty, "Project ID not found in session." + "You must restart the process");
-                return View(projects);
+                _context.Projects.Remove(project);
+                await _context.SaveChangesAsync();
             }
-
-            if (projects.Id != ProjectId || ProjectTitle != "Projects/Delete")
-            {
-                // The user is trying to delete a different project than the one that was loaded in the session
-                return RedirectToAction("Index");
-            }
-
-            if (projects != null)
-            {
-                _context.Projects.Remove(projects);
-            }
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool ProjectsExists(int id)
-        {
-            return _context.Projects.Any(e => e.Id == id);
         }
     }
 }
